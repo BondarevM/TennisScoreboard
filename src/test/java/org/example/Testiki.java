5080a50a72dfd4b1f04c1fe280fc9be71fa48f82
Mikhail Bondarev <bondarevmisha99@gmail.com>
2024-04-19 15:40:19 +0700

    init commit

---
+++ src/test/java/org/example/Testiki.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+
+
+
+public class Testiki {
+    @Test
+    void abc(){
+   assertThat(true);
+    }
+}
