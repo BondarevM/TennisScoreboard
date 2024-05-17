490d72863f5931a379f1fe6c225995f980d8398e
Mikhail Bondarev <bondarevmisha99@gmail.com>
2024-05-17 11:18:15 +0700

    bug fixed

--- src/main/java/com/bma/servlet/IndexServlet.java
+++ src/main/java/com/bma/servlet/IndexServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(value = {"/index", "/"})
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
